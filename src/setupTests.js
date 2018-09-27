24486cad735ca27f7e2675da8eb559d4351b3950
FlindianaJones <FlindianaJones@gmail.coM>
2018-09-27 15:51:33 -0600

    setup packages and folders

---
+++ src/setupTests.js
@@ -0,0 +1,4 @@
+import { configure } fomr 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+configure({ adapter: new Adapter() })
---
+++ src/test/MessageList.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import MessageList from '../components/MessageList';
+
+xit('renders without crashing', () => {
+  //const div = document.createElement('div');
+  //ReactDOM.render(<App />, div);
+  //ReactDOM.unmountComponentAtNode(div);
+});
