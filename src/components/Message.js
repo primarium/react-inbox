9e463644900edb2d3724245c123db6258a463cb2
FlindianaJones <FlindianaJones@gmail.coM>
2018-09-28 10:17:15 -0600

    Added message classes & html

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,34 @@
 import React from 'react';
 
 export default function Message({ message }) {
-    let className = 'message'
+    const starredClass = message.starred ? 'fa-star' : 'fa-star-o'
+    let className = 'row message ' + (message.read ? 'read' : 'unread')
+    let checkbox
+    if (message.selected) {
+        checkbox = <input type="checkbox" checked="checked" />
+        className += ' selected'
+    } else {
+        checkbox = <input type="checkbox" />
+    }
     return (
         <div className={className}>
-            {message.subject}
+            <div className="col-xs-1">
+                <div className="row">
+                    <div className="col-xs-2">
+                        {checkbox}
+                    </div>
+                    <div className="col-xs-2">
+                        <i className={`star fa ${starredClass}`}></i>
+                    </div>
+                </div>
+            </div>
+            <div className="col-xs-11">
+                {message.labels.map((label, index) => <span key={index} className="label label-warning">{label}</span>)}
+                <a href="#">
+                    {message.subject}
+                </a>
+            </div>
         </div>
     )
 }
+
--- src/test/Message.test.js
+++ src/test/Message.test.js
@@ -4,10 +4,60 @@ import initialState from '../initialData'
 import Message from '../components/Message';
 
 describe('message tests', () => {
-    it('renders without crashing', () => {
+    it('message has correct classes and text', () => {
         const message = shallow(<Message message={initialState[0]} />)
 
         expect(message.text()).toContain(initialState[0].subject)
-        expect(message.hasClass('message')).toBeTruthy()
+        expect(message.hasClass('message')).toBeTruthy() // truthiness is dumb
     });
+
+    it('unread message has correct classes', () => {
+        const unreadMessage = shallow(<Message message={initialState[0]} />)
+
+        expect(unreadMessage.hasClass('unread')).toBeTruthy()
+    });
+
+    it('read message has correct classes', () => {
+        const readMessage = shallow(<Message message={initialState[3]} />)
+
+        expect(readMessage.hasClass('read')).toBeTruthy()
+    });
+
+    it('starred message has correct classes', () => {
+        const starredMessage = shallow(<Message message={initialState[0]} />)
+
+        expect(starredMessage.find('.fa-star')).toHaveLength(1)
+    });
+
+    it('unstarred message has correct classes', () => {
+        const unstarredMessage = shallow(<Message message={initialState[3]} />)
+
+        expect(unstarredMessage.find('.fa-star-o')).toHaveLength(1)
+    });
+
+    it('unselected message has correct classes and checkbox is not checked', () => {
+        const unselectedMessage = shallow(<Message message={initialState[0]} />)
+
+        expect(unselectedMessage.hasClass('selected')).toBeFalsy()
+        expect(unselectedMessage.find('input').props().type).toEqual('checkbox')
+        expect(unselectedMessage.find('input').props()).not.toHaveProperty('checked')
+    });
+
+    it('selected message has correct classes and checkbox is checked', () => {
+        const selectedMessage = shallow(<Message message={initialState[3]} />)
+
+        expect(selectedMessage.hasClass('selected')).toBeTruthy()
+        expect(selectedMessage.find('input').props().type).toEqual('checkbox')
+        expect(selectedMessage.find('input').props()).toHaveProperty('checked', 'checked')
+    });
+
+    it('displays the correct labels', () => {
+        const message = shallow(<Message message={initialState[0]} />)
+
+        const labels = message.find('.label')
+        expect(labels).toHaveLength(initialState[0].labels.length)
+        labels.forEach((label, index) => {
+            expect(label.text()).toEqual(initialState[0].labels[index])
+        })
+    })
 })
